c33f2630ee0cca20f8aefb9a5db10c2949c1f291
AlexFerret <42754528+AlexFerret@users.noreply.github.com>
2019-06-25 17:33:29 -0500

    0.1
    revision

---
+++ funciones.js
@@ -0,0 +1,6 @@
+function cambiarColor(){
+	fondo = document.getElementById("fondo1");
+	ventana = document.getElementById("C3");
+	fondo.style.visibility = "visible";
+	ventana.style.visibility ="visible";
+}
