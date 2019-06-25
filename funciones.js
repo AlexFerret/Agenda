07c48676386519a5ab97993094393cf1e5a9d1df
AlexFerret <42754528+AlexFerret@users.noreply.github.com>
2019-06-25 18:21:30 -0500

    0.2

--- funciones.js
+++ funciones.js
@@ -4,3 +4,10 @@ function cambiarColor(){
 	fondo.style.visibility = "visible";
 	ventana.style.visibility ="visible";
 }
+
+function cancelar(){
+	fondo = document.getElementById("fondo1");
+	ventana = document.getElementById("C3");
+	fondo.style.visibility = "hidden";
+	ventana.style.visibility ="hidden";
+}
